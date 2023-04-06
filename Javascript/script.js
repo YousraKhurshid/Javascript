138495b05523ac84443f3f47433801deb0a20d4f
Yousra Khurshid <YousraKhurshid1@gmail.com>
2023-04-07 00:18:54 +0500

    YOUR MESSAGE

---
+++ Javascript/script.js
@@ -0,0 +1,101 @@
+// Class 1 Assign 1 6th April
+//Chap 1
+alert ( "Thanks for your input!" );
+alert ( "Thanks for your input!" );
+//Chap2
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+alert("Thanks for your input!");
+var thanx = "Thanks for your input!"
+alert(thanx);
+//chap3
+var weight = 150;
+var originalNum = 23;
+var newNum = originalNum + 7;
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+var originalNum = 90;
+originalNum = originalNum + 10;
+var originalNum = "23";
+var newNum = originalNum + 7;
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+// var 1stPresident = "Washington";
+var prezWhoCame1st = "Washington";
+alert(144);
+var caseQty = 144;
+alert(caseQty);
+//chap4
+userResponse
+userResponseTime
+userResponseTimeLimit
+response
+//chap5
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+var num = 10;
+var popularNumber = num + 200;
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+var whatsLeftOver = 10 % 3;
+var whatsLeftOver = 9 % 3;
+//chap6
+num++;
+num = num + 1;
+num--;
+var num = 1;
+var newNum = num++;
+var num = 1;
+var newNum = ++num;
+var num = 1;
+var newNum = num--;
+var num = 1;
+var newNum = --num;
+//chap7
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+//chap8
+alert("Thanks for your input!");
+var message = "Thanks for your input!";
+alert(message);
+alert("Thanks, " + userName + "!");
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
+alert("Thanks, " + "Susan" + "!");
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+alert("2" + "2");
+alert("2 plus 2 equals " + 2 + 2);
+//chap9
+var spec = prompt("Your species?", "human");
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
